cbc08eb518be923e24d1fcd9ceb1652bd60c9005
Pankaj-Surya <pnkjsrvnsh@gmail.com>
2022-12-22 00:55:54 +0530

    done navbar design

--- ECOMMERCE/notes.md
+++ ECOMMERCE/notes.md
@@ -1,6 +1,28 @@
-
+## Configuration ##
 1.First Clone repo
 git clone --single-branch -b react-mini https://github.com/safak/youtube.git
 2.install yarn and check version
 3.install all node modules - yarn / npm i
 5.run - yarn start
+6.yarn add styled-components
+
+## Styled Components ##
+* Normal Html and css
+  <div>
+  Navbar     
+  </div>
+* styled components
+   const Container = styled.div`
+   height : 60px;
+  background-color : black;
+  `
+   <Container>Navbar</Container>
+* in styled component we can give same className for
+  every component like for 
+  1.home : className = container
+  2.Login : className = container
+  but in normal html we cant give same name it will override in child
+
+## To divide the left,right and center in equal part
+    even left container have more content like text it should not enter into other contaier
+* give : flex : 1 -> 1 unit to every container
---
+++ ECOMMERCE/src/components/Navbar.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search';
+import Badge from '@mui/material/Badge';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+
+const Container = styled.div`
+height : 60px;
+`
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+ `
+
+const Left = styled.div`
+  flex : 1;
+  display: flex;
+  align-items: center;
+  /* border: 0.5px solid green; */
+ `
+
+const Language = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+ border: 1px solid lightgrey;
+ display: flex;
+ align-items: center;
+ margin-left: 25px;
+ padding: 5px;
+
+`
+const Input = styled.input`
+border: none;
+`
+
+ const Center = styled.div`
+  flex : 1;
+  text-align: center;
+  /* border: 0.5px solid orange; */
+ `
+ const Logo = styled.h1`
+ font-weight: bold;
+ `
+ const Right = styled.div`
+  flex : 1;  
+  display: flex;
+  justify-content: flex-end;
+  /* border: 0.5px solid green; */
+`
+
+const MenuItem = styled.div`
+ font-size : 14px;
+ cursor: pointer;
+ margin-left: 25px;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+        <Wrapper>
+           <Left>
+            <Language>EN</Language>
+            <SearchContainer>
+               <Input/>
+              <SearchIcon style={{color:"grey",fontSize:"16px"}}/>
+            </SearchContainer>
+           </Left>
+           <Center>
+            <Logo>Lama</Logo>
+           </Center>
+            <Right>
+                <MenuItem>REGISTER</MenuItem>
+                <MenuItem>SIGN IN</MenuItem>
+                <MenuItem>
+                <Badge badgeContent={4} color="secondary">
+                  <ShoppingCartOutlinedIcon/>
+                </Badge>
+                </MenuItem>
+            </Right>
+        </Wrapper>
+    </Container>
+  )
+}
+
+export default Navbar
